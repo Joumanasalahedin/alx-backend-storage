b7f071e0e4b377a7ce2b699a9a86580fb75c758f
Joumana <joumanasalahedin@gmail.com>
2024-06-16 02:10:15 +0200

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -9,7 +9,7 @@ BEGIN
     -- Compute the average score
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
 
     -- Update the average score in the users table
     UPDATE users
